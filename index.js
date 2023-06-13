b17d64223fe390d33830d08f5337bd90f68b8b84
nelius <nelius.wandia@student.moringaschool.com>
2023-06-13 12:04:44 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    
+   return Math.abs(blocks - 42)
+}
+
+function distanceFromHqInFeet(blocks){
+
+    return distanceFromHqInBlocks(blocks) * 264
+}
+
+function distanceTravelledInFeet(start , end){
+
+    return Math.abs(start - end)* 264
+}
+
